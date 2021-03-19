bfe2dd8805cab040ed08ed5ce87c95d08eed8a6f
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-18 20:39:48 -0700

    add a callback function to toggle the display of the registration modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Coffee Hero';
+
+  @Input() toggleModal: Function;
+
+  display = false;
+  onToggleClick() {
+    console.log(this.display);
+    this.display = !this.display;
+
+    this.display
+      ? document.querySelector('.registration-modal').classList.remove('hidden')
+      : document.querySelector('.registration-modal').classList.add('hidden');
+  }
 }
