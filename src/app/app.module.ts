b4382923dbf4cf223817a916a2bafab6de4738b7
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-18 20:40:51 -0700

    add single form input and registration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignOnComponent } from './sign-on/sign-on.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { FormFieldComponent } from './form-field/form-field.component';
 
 @NgModule({
-  declarations: [AppComponent, SignUpComponent, SignOnComponent],
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    SignOnComponent,
+    RegistrationFormComponent,
+    FormFieldComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
