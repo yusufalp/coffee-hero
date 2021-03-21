e58b813cb1646578f15ebf88da4fbc0850cfc37c
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-20 21:01:15 -0700

    remove deleted component imports and import app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignOnComponent } from './sign-on/sign-on.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
-import { FormFieldComponent } from './form-field/form-field.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignUpComponent,
-    SignOnComponent,
-    RegistrationFormComponent,
-    FormFieldComponent,
-  ],
-  imports: [BrowserModule],
+  declarations: [AppComponent, SignOnComponent, RegistrationFormComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
