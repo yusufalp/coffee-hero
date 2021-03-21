7b36d17d626f77f30513e1bc9f0801c3fda5d445
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-20 20:59:21 -0700

    moved the modal toggle function to a different component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
   title = 'Coffee Hero';
-
-  @Input() toggleModal: Function;
-
-  display = false;
-  onToggleClick() {
-    console.log(this.display);
-    this.display = !this.display;
-
-    this.display
-      ? document.querySelector('.registration-modal').classList.remove('hidden')
-      : document.querySelector('.registration-modal').classList.add('hidden');
-  }
 }
