6882dc36fa5a6b5a4fd84f08b27a169a99c0a63c
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-20 21:07:27 -0700

    insert password hide/show toggle and validation fucntions

--- src/app/sign-on/sign-on.component.ts
+++ src/app/sign-on/sign-on.component.ts
@@ -1,31 +1,73 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sign-on',
   templateUrl: './sign-on.component.html',
   styleUrls: ['./sign-on.component.css'],
 })
-export class SignOnComponent {
+export class SignOnComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
 
-  @ViewChild('input') password: ElementRef;
-  @ViewChild('passwordToggleText') passwordToggleText: ElementRef;
-  @ViewChild('passwordToggleIcon') passwordToggleIcon: ElementRef;
+  display = false;
+  onClick() {
+    this.display = !this.display;
 
-  onClickTogglePassword = (args: any): void => {
+    this.display
+      ? document.querySelector('.registration-modal').classList.remove('hidden')
+      : document.querySelector('.registration-modal').classList.add('hidden');
+  }
+
+  @ViewChild('password') password: ElementRef;
+  @ViewChild('passwordFeatureText') passwordFeatureText: ElementRef;
+  @ViewChild('passwordFeatureIcon') passwordFeatureIcon: ElementRef;
+
+  onClickTogglePassword() {
     let passwordType = this.password.nativeElement.type;
 
     if (passwordType === 'password') {
       this.password.nativeElement.type = 'text';
-      this.passwordToggleText.nativeElement.textContent = ' Hide';
-      this.passwordToggleIcon.nativeElement.src =
+      this.passwordFeatureText.nativeElement.textContent = ' Hide';
+      this.passwordFeatureIcon.nativeElement.alt = 'closed eye icon';
+      this.passwordFeatureIcon.nativeElement.src =
         './assets/icon--eye-closed.svg';
     } else {
       this.password.nativeElement.type = 'password';
-      this.passwordToggleText.nativeElement.textContent = ' Show';
-      this.passwordToggleIcon.nativeElement.src = './assets/icon--eye-open.svg';
+      this.passwordFeatureText.nativeElement.textContent = ' Show';
+      this.passwordFeatureIcon.nativeElement.alt = 'open eye icon';
+      this.passwordFeatureIcon.nativeElement.src =
+        './assets/icon--eye-open.svg';
     }
-  };
+  }
+
+  error: boolean = false;
+  errorMessage: string = '';
+  alertIcon: string = './assets/icon--alert.svg';
+
+  onChangeReceiveValue(event) {
+    let value = event.target.value;
+
+    let checkUpperCase = /[A-Z]/g;
+    let checkNumber = /[0-9]/g;
+    let letterResult = checkUpperCase.test(value);
+    let numberResult = checkNumber.test(value);
+
+    if (event.target.id === 'password') {
+      this.error = true;
+      if (value.length < 10) {
+        this.errorMessage = 'Password must be at least 10 characters!';
+        if (value.length === 0) {
+          this.errorMessage = 'No value provided';
+        }
+      } else {
+        if (!(letterResult && numberResult)) {
+          this.errorMessage =
+            'Password must include at least one uppercase and one number!';
+          return;
+        }
+        this.error = false;
+      }
+    }
+  }
 }
